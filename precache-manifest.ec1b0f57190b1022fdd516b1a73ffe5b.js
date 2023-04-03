9f70b71aec8c255ee57cd7f7973141043c3d0b99
Code Sandbox <hello@codesandbox.io>
2023-04-03 08:37:57 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ec1b0f57190b1022fdd516b1a73ffe5b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba9ca747437ff6d7a687ccaaba98e2f",
+    "url": "https://ejicash.github.io/csb-lez2m2/index.html"
+  },
+  {
+    "revision": "ad36e0aba4588e846032",
+    "url": "https://ejicash.github.io/csb-lez2m2/static/js/2.05d131db.chunk.js"
+  },
+  {
+    "revision": "c1326ed83be0a92b2aff",
+    "url": "https://ejicash.github.io/csb-lez2m2/static/js/main.c06cec4c.chunk.js"
+  },
+  {
+    "revision": "5c3c8e2bc8209d36e865",
+    "url": "https://ejicash.github.io/csb-lez2m2/static/js/runtime-main.d227acc8.js"
+  }
+]);
